e30510fd875eb246001b552283c42753ed0a1174
santaimpersonator <36016723+santaimpersonator@users.noreply.github.com>
2026-04-13 04:34:50 -0600

    Enable Iconify icons
    Enable the use of the Iconify icons

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,17 @@ import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
 
+
+// ==========================
+// External Component Imports
+// ==========================
+
+// Iconify setup
+// Run `npm install @iconify/react` to install the Iconify React component.
+import { Icon } from '@iconify/react'; // Import the entire Iconify library.
+
+
+
 // ========================
 // Custom Component Imports
 // ========================
@@ -16,5 +27,6 @@ import DocCardList from '@theme/DocCardList';
 export default {
   // Re-use the default mapping
   ...MDXComponents,
+  Icon: Icon, // Make the iconify Icon component available in MDX as <icon />.
   DocCardList, // Make the DocCardList component available in MDX as <DocCardList />.
 };
